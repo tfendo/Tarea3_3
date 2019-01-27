9fb9876af967c5b0b5a3fe9c29feb6b0925e1bef
Hugo Guillín <hugoguillin@hotmail.com>
2019-01-27 18:12:39 +0100

    Task_2

--- app/src/main/java/com/hugoguillin/hellocompat/MainActivity.java
+++ app/src/main/java/com/hugoguillin/hellocompat/MainActivity.java
@@ -2,12 +2,35 @@ package com.hugoguillin.hellocompat;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView text;
+    private String[] mColorArray = {"red", "pink", "purple", "deep_purple",
+            "indigo", "blue", "light_blue", "cyan", "teal", "green",
+            "light_green", "lime", "yellow", "amber", "orange", "deep_orange",
+            "brown", "grey", "blue_grey", "black" };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        text = findViewById(R.id.hello_textview);
+        if (savedInstanceState != null){
+            text.setTextColor(savedInstanceState.getInt("color"));
+        }
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        // save the current text color
+        outState.putInt("color", text.getCurrentTextColor());
+    }
+
+    public void changeColor(View view) {
+
     }
 }
