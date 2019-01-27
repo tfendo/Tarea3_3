6eb970d08ea6f50e5b6d1892ff21584ff563fdad
Hugo Guillín <hugoguillin@hotmail.com>
2019-01-27 18:31:00 +0100

    Task_3

--- app/src/main/java/com/hugoguillin/hellocompat/MainActivity.java
+++ app/src/main/java/com/hugoguillin/hellocompat/MainActivity.java
@@ -1,9 +1,12 @@
 package com.hugoguillin.hellocompat;
 
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -31,6 +34,11 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void changeColor(View view) {
-
+        Random random = new Random();
+        String color = mColorArray[random.nextInt(20)];
+        int recurso = getResources().getIdentifier(color, "color",
+                getApplicationContext().getPackageName());
+        int colorRecurso = ContextCompat.getColor(this,recurso);
+        text.setTextColor(colorRecurso);
     }
 }
